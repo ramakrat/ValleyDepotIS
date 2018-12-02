a9db8c32ee77a51648739097e5f73546d660e302
lamainbe <44983747+lamainbe@users.noreply.github.com>
2018-12-02 16:28:45 -0500

    Update ValleyDepotFX.java

--- ValleyDepotFX.java
+++ ValleyDepotFX.java
@@ -109,6 +109,71 @@ public class ValleyDepotFX extends Application {
         
       
     }
+    Label inventoryLabel = new Label("Manage Inventory System");
+        Button createInventory = new Button("Create new Inventory Item");
+        Button editInventory = new Button("Edit an inventory Item");
+        GridPane inventoryPane1 = new GridPane();
+        Label inv1 = new Label("1.)");
+        Label inv2 = new Label("2.)");
+        Label inv3 = new Label("3.)");
+        Button exit = new Button("Return to Main Menu");
+        inventoryPane1.add(inventoryLabel, 1, 0);
+        inventoryPane1.add(createInventory, 1, 1);
+        inventoryPane1.add(editInventory, 1, 2);
+        inventoryPane1.add(inv1, 0, 1);
+        inventoryPane1.add(inv2, 0, 2);
+        inventoryPane1.add(inv3, 0, 3);
+        inventoryPane1.add(exit, 1,3);
+        Scene scene2 = new Scene(inventoryPane1, 400, 400);
+        
+        //manage inventory button
+        btn2.setOnAction(e -> {
+            primaryStage.setTitle("Manage Inventory Items");
+            primaryStage.setScene(scene2);
+            primaryStage.show();
+            inventoryPane1.setHgap(10);
+            inventoryPane1.setVgap(10);
+        });
+          
+        //create a sale
+          Label saleLabel = new Label("Create a Sale");
+          GridPane salesPane1 = new GridPane();
+          Label sale1 = new Label("Item Name ");
+          TextField name = new TextField();
+          Label sale2 = new Label("Item Weight");
+          TextField weight = new TextField();
+          Label sale3 = new Label("Item Description");
+          TextField description = new TextField();
+          Label sale4 = new Label("Item Price");
+          Label sale5 = new Label("Item For-Sale Price");
+          TextField price = new TextField();
+          TextField forSale = new TextField();
+          Label sale6 = new Label("Quantity of Items Available");
+          TextField quantity = new TextField();
+          Button leave = new Button("Return to Main Menu");
+          salesPane1.add(saleLabel, 1, 0);
+          salesPane1.add(sale1, 0, 1);
+          salesPane1.add(name, 1, 1);
+          salesPane1.add(sale2, 0, 2);
+          salesPane1.add(weight, 1, 2);
+          salesPane1.add(sale3, 0, 3);
+          salesPane1.add(description, 1, 3);
+          salesPane1.add(sale4, 0, 4);
+          salesPane1.add(price, 1, 4);
+          salesPane1.add(sale5, 0, 5);
+          salesPane1.add(forSale, 1, 5);
+          salesPane1.add(sale6, 0, 6);
+          salesPane1.add(quantity, 1, 6);
+          Scene scene3 = new Scene(salesPane1, 400, 400);
+          
+          //create a sale button
+          btn3.setOnAction(e -> {
+            primaryStage.setTitle("Create a Sale");
+            primaryStage.setScene(scene3);
+            primaryStage.show();
+            inventoryPane1.setHgap(10);
+            inventoryPane1.setVgap(10);
+        });
 
     /**
      * @param args the command line arguments
